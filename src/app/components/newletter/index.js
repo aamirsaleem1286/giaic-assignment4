8e5af0626f57b2556dc38aac54d61e04cfeb9d16
Aamir Saleem <65162736+aamirsaleem1286@users.noreply.github.com>
2024-11-19 15:31:40 +0500

    Update index.js

--- src/app/components/newletter/index.js
+++ src/app/components/newletter/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Newletter = () => {
   return (
-    <div className='bg-black h-[38px] flex gap-200'>
+    <div className='bg-black h-[38px] flex justify-around items-center text-bold'>
       <h1 className='text-white text-xl text-center'>Sign up and get 20% off to your first order. Sign Up Now</h1>
-    <Image src="/images/cross.png" alt='image' width={80} height={80}/>
+    <Image src="/images/cross.png" alt='image' width={40} height={25}/>
     </div>
   )
 }
